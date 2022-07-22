168f0fb6aa081ff1c9e3b2edf7ce73fb72f55339
nasim67reja <nasimreja67@gmail.com>
2022-07-21 17:34:14 -0700

    Updated

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+
+import ButtonC from "./Components/Button";
+export default function App() {
+  return (
+    <Fragment>
+      <ButtonC />
+    </Fragment>
+  );
+}
---
+++ src/Components/Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "@mui/material/Button";
+
+const ButtonC = () => {
+  return <Button variant="contained">Hello World</Button>;
+};
+
+export default ButtonC;
